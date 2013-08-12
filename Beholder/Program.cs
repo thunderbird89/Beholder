b23c9ee4d8a7c42f171b88f4ed340ebd8fa28bdd
thunderbird89 <zalanmeggyesi@yahoo.com>
2013-08-13 00:59:46 +0200

    started work on buffering queues

--- Beholder/Program.cs
+++ Beholder/Program.cs
@@ -7,42 +7,67 @@ using messaging;
 
 namespace Beholder
     {
-    class Program
+    public struct bufferQueue
         {
-        static void Main(string[] args)
+
+        private string _datagram;
+
+        public string data
             {
-            int meh;
-
-            Boolean breakFlag = false;
-            while (!breakFlag)
+            get
                 {
-                // Parse input buffer into variables for future work
-                string buffer = " ";
-                string[] inputs = buffer.Split(new Char[] { '.' });
-                string sensorID = inputs[0];
-                string sensorLocation = inputs[1];
-                string sensorReading = inputs[2];
+                return _datagram;
+                }
+            set
+                {
+                this._datagram = value;
+                }
+            }
 
-                // Create new sensor node objects, name, transmit message
-                MessageQueuing.SensorNode n1 = new MessageQueuing.SensorNode();
-                n1.setname(sensorID);
-                n1.sendID(n1.name);
-                n1.setlocation(sensorLocation);
-                n1.sendLocation(n1.location);
-                n1.readValue(sensorReading);
-                n1.sendMessage(n1.reading);
+        public bufferQueue(string datagram)
+            {
+            _datagram = datagram;
+            }
+        }
 
-                // start processing queue
-                MessageQueuing.CentralMessageProcessor cmp = new MessageQueuing.CentralMessageProcessor();
-                cmp.startProcessing();
 
-                meh = Console.Read();
-                string ch = Convert.ToString(meh);
-                if (ch == "x")
-                    breakFlag = true;
-                if (breakFlag == true)
-                    break;
+        class Program
+            {
+            static void Main(string[] args)
+                {
+                int meh;
+
+                Boolean breakFlag = false;
+                while (!breakFlag)
+                    {
+                    //function to retrieve data from the serial interface, into buffer
+                    string buffer = " ";
+
+                    string[] inputs = buffer.Split(new Char[] { '.' });
+                    string sensorID = inputs[0];
+                    string sensorLocation = inputs[1];
+                    string sensorReading = inputs[2];
+
+                    // Create new sensor node objects, name, transmit message
+                    MessageQueuing.SensorNode n1 = new MessageQueuing.SensorNode();
+                    n1.setname(sensorID);
+                    n1.sendID(n1.name);
+                    n1.setlocation(sensorLocation);
+                    n1.sendLocation(n1.location);
+                    n1.readValue(sensorReading);
+                    n1.sendMessage(n1.reading);
+
+                    // start processing queue
+                    MessageQueuing.CentralMessageProcessor cmp = new MessageQueuing.CentralMessageProcessor();
+                    cmp.startProcessing();
+
+                    meh = Console.Read();
+                    string ch = Convert.ToString(meh);
+                    if (ch == "x")
+                        breakFlag = true;
+                    if (breakFlag == true)
+                        break;
+                    }
                 }
             }
         }
-    }
