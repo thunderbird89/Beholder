35cad7ff78623399eb3c4e98cadf51d035b519db
thunderbird89 <zalanmeggyesi@yahoo.com>
2013-08-30 00:50:31 +0200

    Cleanup
    Renamed files for clarity
    Added placeholders on vision and spectroscopy

---
+++ Beholder/Spectroscopy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using messagesorting;
+
+namespace spectro
+{
+    public class comms
+    {
+        // Receive data method
+        // Database query method
+        // Send result method
+    }
+
+    public class fingerprinting
+    {
+        // Fingerprinting algorhythm
+        // Fuzzy logic filtering?
+    }
+}
---
+++ Beholder/Vision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using messagesorting;
+
+namespace vision
+{
+    public class visionRendering
+    {
+        public void renderVideo
+        {
+            //Hardcore rendering code
+        }
+    }
+}
